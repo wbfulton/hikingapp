0694f68fdd6de820d907b5c7d257264396cc2b55
Will Fulton <wbfulton@me.com>
2019-09-03 12:51:17 -0700

    implementing jwt

--- config/db.js
+++ config/db.js
@@ -6,7 +6,8 @@ const db = config.get('mongoURI'); // gets mongoDB URI to connect
 const connectDB = async () => {
     try {
         await mongoose.connect(db, { 
-            useNewUrlParser: true
+            useNewUrlParser: true,
+            useCreateIndex: true
         });
 
         console.log('MongoDb Connected...')
