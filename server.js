4a552317b6695691bb544dabd302f699d584f0c0
Will Fulton <wbfulton@me.com>
2019-10-01 23:45:59 -0700

    Deployment Prepare

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express'); // imports express
 const connectDB = require('./config/db'); // for connecting db
+const path = require('path'); // for manipulating path routing
 
 const app = express(); // creates instance of express
 
@@ -9,19 +10,23 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-/*
-// Get Route for Testing
-app.get('/', (req, res, next) => {
-    res.send('API Running');
-})
-*/
-
 // Routing
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/drives', require('./routes/api/drives'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/users', require('./routes/api/users'));
+
+// Serve static assests in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  // Serves index.html file
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // Port will listen on environment variable first, or 5000
 const PORT = process.env.PORT || 5000;
