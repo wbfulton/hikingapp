de2815d8710cbb33480465f6aabfe291409b9eb0
Will Fulton <wbfulton@me.com>
2019-10-02 16:30:00 -0700

    __dirname, not _dirname. Refresh bug

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === 'production') {
 
   // Serves index.html file
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
